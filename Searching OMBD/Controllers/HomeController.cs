a9b9cb41f42cb288ad0ef4d867a39b1755901b9f
Tee <sotomi.tayo@gmail.com>
2023-02-15 00:47:31 -0600

    Searching OMDB with Bootstrap

---
+++ Searching OMBD/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Searching_OMBD.Models;
+using System.Diagnostics;
+
+namespace Searching_OMBD.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult MovieSearch()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult MovieSearch(string Title)
+        {
+            MovieModel result = MovieDAL.GetMovie(Title);
+            return View(result );
+        }
+        [HttpGet]
+        public IActionResult MovieNight()
+        {
+            return View();
+        }
+        [HttpPost]  
+        public IActionResult MovieNight(string Title1,string Title2,string Title3)
+        {
+            List<MovieModel> result = new List<MovieModel>();
+            result.Add(MovieDAL.GetMovie(Title1));
+            result.Add(MovieDAL.GetMovie(Title2));
+            result.Add(MovieDAL.GetMovie(Title3));
+            return View(result);
+
+            
+        }
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Searching OMBD/Models/MovieDAL.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+
+namespace Searching_OMBD.Models
+{
+    public class MovieDAL
+    {
+        public static MovieModel GetMovie(string input)
+        {
+            string id = "9b6b282b";
+
+            //Adjust here
+            //setup
+            string url = $"https://www.omdbapi.com/?t={input}&apikey={id}";
+            //request
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            //save the response
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            //Convert it to JSON
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string JSON = reader.ReadToEnd();
+
+            //Adjust here
+            //convert to C#
+            MovieModel result = JsonConvert.DeserializeObject<MovieModel>(JSON);
+            return result;
+        }
+    }
+}
